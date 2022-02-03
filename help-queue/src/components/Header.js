aaefc72c0f271fcb964fd2f720c438d108516fcb
Sisi Vieira <cicy886@gmail.com>
2022-02-02 20:21:30 -0800

    attach special styles to header

--- help-queue/src/components/Header.js
+++ help-queue/src/components/Header.js
@@ -1,11 +1,25 @@
 import React from "react";
 import ticketsImage from "./../img/tickets.png";
 import { Link } from "react-router-dom";
+import styled from 'styled-components';
+
+const HelpQueueHeader = styled.h1`
+  font-size: 24px;
+  text-align: center;
+  color: lightblue;
+`;
+
+const StyledWrapper = styled.section`
+  background-color: orange;
+`;
 
 function Header(){
   return (
+    <StyledWrapper>
     <React.Fragment>
-      <h1>Help Queue</h1>
+      <HelpQueueHeader>
+          Help Queue
+        </HelpQueueHeader>
       <ul>
         <li>
           <Link to="/">Home</Link>
@@ -16,6 +30,7 @@ function Header(){
       </ul>
       <img src={ticketsImage} alt="An image of tickets" />
     </React.Fragment>
+    </StyledWrapper>
   );
 }
 
